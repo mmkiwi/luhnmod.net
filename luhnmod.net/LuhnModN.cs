2b173d80797916bf6c8f260de0b271b6a0bb0ad6
Micah Makaiwi <micah@mmkiwi.com>
2019-10-25 11:07:49 -0400

    Using .editorconfig

---
+++ luhnmod.net/LuhnModN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuhnModN
+{
+    public class LuhnModN : LuhnModN<char>
+    {
+        public string ValidChars { get; private set; }
+
+        public override int GetCodePoint(char item)
+        {
+            return ValidChars.IndexOf(item);
+        }
+
+        public override int NumberOfCodePoints { get { return ValidChars.Length; } }
+
+        public override char GetItem(int codePoint)
+        {
+            return ValidChars[codePoint];
+        }
+
+        public LuhnModN(string validChars)
+        {
+            //TODO: Test no duplicates
+            ValidChars = validChars;
+        }
+
+        public char GenerateCheckDigit(string checkString)
+        {
+            return GenerateCheckDigit(checkString);
+        }
+
+        public bool CheckValue(string checkString)
+        {
+            return CheckValue(checkString);
+        }
+    }
+}
